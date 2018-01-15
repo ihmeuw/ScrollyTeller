85de579f8c1c1ac001bb2b8ad3bb5ea9cf471c13
Ryan Shackleton <ryshack@uw.edu>
2018-01-15 14:38:09 -0800

    Add the new example sections to the app, remove unused jquery import and the old ThemeTemplate.

--- app/app.js
+++ app/app.js
@@ -1,16 +1,18 @@
 /* global VizHub, document, window, global window */
 
-import ThemeTemplate from './theme/theme';
 import './scss/style.scss';
-import $ from 'jquery';
-
+import TitleSection from './00_title_section/TitleSection';
+import Section1 from './01_section_1/Section1';
+import SectionExample from './99_section_example/SectionExample';
 
 export default class App {
   constructor() {
-    this.view = new ThemeTemplate(this);
+    const appContainerId = 'app';
+    this.title = new TitleSection(appContainerId, 'title', 'Story Theme Template');
+    this.section1 = new Section1({ appContainerId });
+    this.example = new SectionExample({ appContainerId });
   }
 }
-
 
 $(document).ready(() => {
   new App();
