f522f4fa544c569448f257e0f88d02faa465219b
Ryan Shackleton <ryshack@uw.edu>
2018-01-19 20:13:00 -0800

    Modify examples to work within the new format

---
+++ app/01_example_section_simple/SimpleSection.js
@@ -0,0 +1,69 @@
+import { select } from 'd3';
+import * as d3promise from 'd3.promise';
+
+export default class SimpleSection {
+  constructor({ appContainerId, cssNames }) {
+    /** section object with identifier, narration, and data (for the graph), stored, and returned
+     * to create the state object */
+    this.config = {
+      appContainerId,
+      sectionIdentifier: 'simple',
+      cssNames,
+      /** array of narration objects, OR a promise to return an array of narration objects.
+       * See README for the specfication of the narration objects */
+      narration: d3promise.csv('app/01_example_section_simple/data/narration_section_simple.csv'),
+      // narration: [ {}, ],
+      /** data in a form consumable by user specified graphing methods,
+       * OR a promise to return data. */
+      data: { notEmpty: [] }, // data can't be empty
+      /** promise example... */
+      // data: d3promise.csv('app/99_example_section_chart/data/data-by-series.csv'),
+
+      /** functions that must be implemented/defined */
+      functionBindingContext: this,
+      reshapeDataFunction: this.processData,
+      buildGraphFunction: this.buildChart,
+      onScrollFunction: this.onScroll,
+      onActivateNarrationFunction: this.onActivateNarration,
+
+      /** optional flags to govern spacers and css behavior */
+
+      /** set to true to show spacer sizes for debugging */
+      showSpacers: true,
+      /**  if false, you must specify your own graph css, where
+       * the graph class name is "graph_section_ + sectionIdentifier" */
+      useDefaultGraphCSS: true,
+    };
+
+    return this.config;
+  }
+
+  processData() {
+  }
+
+  buildChart() {
+  }
+
+  onActivateNarration(index, activeNarrationBlock) {
+  }
+
+  onScroll(index, progress, activeNarrationBlock) {
+    /** use trigger specified in the narration csv file to trigger actions */
+    switch (activeNarrationBlock.getAttribute('trigger')) {
+      case 'unhide':
+        /** set graph opacity based on progress to fade graph in */
+        select(`#${this.myGraphId()}`).style('opacity', progress - 0.05);
+        break;
+      case 'hide':
+        /** set graph opacity based on progress to fade graph out */
+        select(`#${this.myGraphId()}`).style('opacity', 0.9 - progress);
+        break;
+      default:
+        select(`#${this.myGraphId()}`).style('opacity', 1);
+    }
+  }
+
+  myGraphId() {
+    return this.config.cssNames.graphId(this.config.sectionIdentifier);
+  }
+}
---
+++ app/99_example_section_chart/ExampleChartSection.js
@@ -0,0 +1,140 @@
+import {
+  get,
+  isUndefined,
+  groupBy,
+  toNumber,
+  toArray,
+  isEmpty,
+  reduce,
+} from 'lodash';
+import { select, timeParse } from 'd3';
+import * as d3promise from 'd3.promise';
+import SampleChart from './components/template.chart';
+
+export default class ExampleChartSection {
+  constructor({ appContainerId, cssNames }) {
+    /** section object with identifier, narration, and data (for the graph), stored, and returned
+     * to create the state object */
+    this.config = {
+      appContainerId,
+      sectionIdentifier: 'example',
+      cssNames,
+      /** array of narration objects, OR a promise to return an array of narration objects.
+       * See README for the specfication of the narration objects */
+      narration: d3promise.csv('app/99_example_section_chart/data/narration_section_chart.csv'),
+      // narration: [ {}, ],
+      /** data in a form consumable by user specified graphing methods,
+       * OR a promise to return data. */
+      // data: { notEmpty: [] }, // data can't be empty
+      data: d3promise.csv('app/99_example_section_chart/data/data-by-series.csv'),
+
+      /** functions that must be implemented/defined */
+      functionBindingContext: this,
+      reshapeDataFunction: this.processData,
+      buildGraphFunction: this.buildChart,
+      onScrollFunction: this.onScroll,
+      onActivateNarrationFunction: this.onActivateNarration,
+
+      /** optional flags to govern spacers and css behavior */
+      /** set to true to show spacer sizes for debugging */
+      showSpacers: false,
+      /**  if false, you must specify your own graph css, where
+       * the graph class name is "graph_section_ + sectionIdentifier" */
+      useDefaultGraphCSS: false,
+    };
+
+    return this.config;
+  }
+
+  processData(results) {
+    /** using d3promise to convert d3.csv calls to promises */
+    const parseTime = timeParse('%y');
+    /** parse results and convert dates to years, close to number */
+    const dataProcessed = results.map((datum) => {
+      return {
+        series: datum.series,
+        date: parseTime(datum.date),
+        close: toNumber(datum.close),
+      };
+    });
+    /** set this.data to the results array and handle some date and number conversion */
+    return groupBy(dataProcessed, 'series');
+  }
+
+  getFilteredDataByTriggerString(trigger) {
+    /** assume trigger is formatted as follows:
+     * series:yearstart-yearend,series:yearstart-yearend
+     */
+    const seriesYearRangeArray = trigger.split(',');
+    const allDataFiltered = reduce(seriesYearRangeArray, (accum, seriesYearRange) => {
+      const [series, yearRange] = seriesYearRange.split(':');
+      if (!isEmpty(series) && !isEmpty(yearRange)) {
+        // TODO: check that years are in valid range?
+        const dataSeries = get(this.config.data, series, undefined);
+        if (!isUndefined(dataSeries)) {
+          const parseTime = timeParse('%y');
+          const yearArray = yearRange.split('-');
+          const [yearStartDate, yearEndDate] = yearArray.map(parseTime);
+          const filteredData = dataSeries.filter((datum) => {
+            return datum.date >= yearStartDate && datum.date <= yearEndDate;
+          });
+          if (!isEmpty(filteredData)) {
+            return accum.concat(filteredData);
+          }
+        }
+      }
+    }, []);
+
+    return toArray(groupBy(allDataFiltered, 'series'));
+  }
+
+  buildChart() {
+    this.chart = new SampleChart({
+      container: `#${this.myGraphId()}`,
+    });
+    this.chart.render(this.getFilteredDataByTriggerString('series1:90-17'));
+  }
+
+  onActivateNarration(index, activeNarrationBlock) {
+    const trigger = activeNarrationBlock.getAttribute('trigger');
+
+    switch (trigger) {
+      case 'unhide':
+      case 'hide':
+      case 'opacityzero':
+        break; // do nothing for unhide, hide, opacity zero
+      default: {
+        const filteredData = this.getFilteredDataByTriggerString(trigger);
+        if (!isEmpty(filteredData)) {
+          filteredData.forEach((datum) => {
+            this.chart.update(datum);
+          });
+        }
+      }
+    }
+  }
+
+  onScroll(index, progress, activeNarrationBlock) {
+    /** use trigger specified in the narration csv file to trigger actions */
+    switch (activeNarrationBlock.getAttribute('trigger')) {
+      case 'unhide':
+        /** set graph opacity based on progress to fade graph in */
+        select(`#${this.myGraphId()}`).style('opacity', progress - 0.05);
+        break;
+      case 'hide':
+        /** set graph opacity based on progress to fade graph out */
+        select(`#${this.myGraphId()}`).style('opacity', 0.9 - progress);
+        break;
+      case 'opacityzero':
+        /** set opacity to zero (after fadeout */
+        select(`#${this.myGraphId()}`).style('opacity', 0);
+        break;
+      default:
+        select(`#${this.myGraphId()}`).style('opacity', 1);
+    }
+  }
+
+  myGraphId() {
+    return this.config.cssNames.graphId(this.config.sectionIdentifier);
+  }
+}
