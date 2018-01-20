24f181a674641b52ba4908e9a8ca3ae99f3eb4d1
Ryan Shackleton <ryshack@uw.edu>
2018-01-19 20:12:33 -0800

    modify app to build state and the DataStory

--- app/app.js
+++ app/app.js
@@ -1,16 +1,48 @@
+import {
+  keyBy,
+} from 'lodash';
+
 import './scss/style.scss';
 import TitleSection from './00_title_section/TitleSection';
-import SectionSimple from './01_example_section_simple/SectionSimple';
-import SectionExample from './99_example_section_chart/SectionExampleChart.js';
+import DataStory from './DataStory/DataStory';
+
+import SimpleSection from './01_example_section_simple/SimpleSection';
+import ExampleChartSection from './99_example_section_chart/ExampleChartSection';
+import ScrollyTellerNames from './ScrollyTeller/utils/ScrollyTellerNames';
 
 export default class App {
   constructor() {
-    const containerSelector = { appContainerId: 'app' };
-    const titleSectionProps = { titleTextCSS: 'title', titleText: 'Scrolly Teller Example' };
+    /** css naming handler for ScrollyTeller
+     * optionally override any scrollyteller css variable names
+     * from ScrollyTeller/css_utils/name_defaults.js */
+    const cssNames = new ScrollyTellerNames();
+    const appContainerId = 'app';
 
-    this.title = new TitleSection({ ...containerSelector, ...titleSectionProps });
-    this.sectionSimple = new SectionSimple(containerSelector);
-    this.sectionWithChart = new SectionExample(containerSelector);
+    /** build a list of story sections.  Constructors should return a valid configuration */
+
+    const simple = new SimpleSection({ appContainerId, cssNames }); // returns it own config
+    const example = new ExampleChartSection({ appContainerId, cssNames });
+    const storySections = [
+      simple,
+      example,
+    ];
+
+    this.state = {
+      appContainerId,
+      cssNames,
+      titleSectionProps: {
+        titleTextCSS: 'title',
+        titleText: 'Scrolly Teller Example',
+      },
+      sectionList: keyBy(storySections, 'sectionIdentifier'),
+    };
+
+    this.title = new TitleSection({
+      appContainerId: this.state.appContainerId,
+      ...this.state.titleSectionProps,
+    });
+
+    this.dataStory = new DataStory(this.state);
   }
 }
 
