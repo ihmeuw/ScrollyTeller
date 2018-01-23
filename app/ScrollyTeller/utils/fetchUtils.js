55c68eebf62effd29adce013d52238e0ac53feba
Ryan Shackleton <ryshack@uw.edu>
2018-01-23 14:42:52 -0800

    Clean up async functions in fetchUtils

--- app/ScrollyTeller/utils/fetchUtils.js
+++ app/ScrollyTeller/utils/fetchUtils.js
@@ -5,28 +5,6 @@ import {
   isFunction,
   toArray,
 } from 'lodash';
-import { isPromise } from './configValidator';
-
-/**
- *  Looks for already stored data and returns a promise to return the un-modified data,
- *  OR if section.data is already a promise, it returns that promise
- *  @param {string} sectionList: array of sections to parse promises from
- *  @param {string} propertyName: property name to look for in each section (data or narration)
- *  @return {Array} returns an array of promises: 1 for each section in sectionList.
- *  */
-export function getPromisesFromSectionList(sectionList, propertyName) {
-  const sectionArray = toArray(sectionList);
-  /** return a promise for every section so we can used the indices to index into results array
-   * after the promise is resolved */
-  return map(sectionArray, (config) => {
-    /** if the promise is already specified by the user spec, just return it */
-    if (isPromise(get(config, propertyName))) {
-      return get(config, propertyName);
-    }
-    /** otherwise, return a promise to just get the already stored data from the section */
-    return Promise.resolve(get(config, propertyName));
-  });
-}
 
 /**
  * finds any narration promises in the sectionList, resolves them,
@@ -36,20 +14,22 @@ export function getPromisesFromSectionList(sectionList, propertyName) {
  */
 export async function fetchNarration(sectionList) {
   /** fetches: 1 for every section */
-  const fetches = getPromisesFromSectionList(sectionList, 'narration');
-  /** section array: indexed the same as the fetches array, and to the results array */
   const sectionArray = toArray(sectionList);
-  await Promise.all(fetches)
-    .then((results) => {
-      /** results are indexed in the same order as results array */
-      forEach(sectionArray, (config, i) => {
-        const {
-          sectionIdentifier,
-        } = config;
-        /** re-set the new narration from the promise results */
-        sectionList[sectionIdentifier].narration = results[i];
-      });
-    });
+  const fetches = map(sectionArray, (config) => {
+    /** if the promise is already specified by the user spec, just return it
+     * (if property is a promise, resolve will resolve to a Promise) */
+    return Promise.resolve(get(config, 'narration'));
+  });
+  /** section array: indexed the same as the fetches array, and to the results array */
+  const results = await Promise.all(fetches);
+  /** results are indexed in the same order as results array */
+  forEach(sectionArray, (config, i) => {
+    const {
+      sectionIdentifier,
+    } = config;
+    /** re-set the new narration from the promise results */
+    sectionList[sectionIdentifier].narration = results[i];
+  });
 }
 
 /**
@@ -61,23 +41,25 @@ export async function fetchNarration(sectionList) {
  */
 export async function fetchDataAndProcessResults(sectionList) {
   /** fetches: 1 for every section */
-  const fetches = getPromisesFromSectionList(sectionList, 'data');
-  /** section array: indexed the same as the fetches array, and to the results array */
   const sectionArray = toArray(sectionList);
-  await Promise.all(fetches)
-    .then((results) => {
-      /** results are indexed in the same order as results array */
-      forEach(sectionArray, (config, i) => {
-        const {
-          sectionIdentifier,
-          reshapeDataFunction,
-          functionBindingContext,
-        } = config;
-        /** call reshapeDataFunction if it is defined and overwrite the new data values */
-        sectionList[sectionIdentifier].data = isFunction(reshapeDataFunction)
-          ? reshapeDataFunction.apply(functionBindingContext, [results[i]])
-          : results[i];
-      });
-    });
+  const fetches = map(sectionArray, (config) => {
+    /** if the promise is already specified by the user spec, just return it
+     * (if property is a promise, resolve will resolve to a Promise) */
+    return Promise.resolve(get(config, 'data'));
+  });
+  /** section array: indexed the same as the fetches array, and to the results array */
+  const results = await Promise.all(fetches);
+  /** results are indexed in the same order as sectionList array */
+  forEach(sectionArray, (config, i) => {
+    const {
+      sectionIdentifier,
+      reshapeDataFunction,
+      functionBindingContext,
+    } = config;
+    /** call reshapeDataFunction if it is defined and overwrite the new data values */
+    sectionList[sectionIdentifier].data = isFunction(reshapeDataFunction)
+      ? reshapeDataFunction.apply(functionBindingContext, [results[i]])
+      : results[i];
+  });
 }
 
