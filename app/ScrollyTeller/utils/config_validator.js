6d2d21d2318175be22e78b565a6494b7291987a5
Ryan Shackleton <ryshack@uw.edu>
2018-01-23 12:27:46 -0800

    Make exampleConfigObject an object, not a function

--- app/ScrollyTeller/utils/config_validator.js
+++ app/ScrollyTeller/utils/config_validator.js
@@ -7,73 +7,71 @@ import {
 } from 'lodash';
 import ScrollyTellerNames from './ScrollyTellerNames';
 
-export function exampleConfigObject() {
-  return {
-    appContainerId: 'sampleConfigId',
-    /** can override the defaults in this class to customize CSS */
-    cssNames: new ScrollyTellerNames(),
-    /** build a list of story sections, keyed by sectionIdentifier.
-     * Each section constructor should return a valid configuration,
-     * or create a new section "object" outside, and add a .config() function
-     * that returns a valid configuration object */
-    sectionList: [
-      {
-        appContainerId: 'sampleConfigId', /** must be the same as the appContainerId above */
-        sectionIdentifier: 'mySectionIdentifier',
-        /** optional cssNames object if any CSS are overridden from the defaults
-         * can override the defaults in this class to customize CSS */
-        cssNames: new ScrollyTellerNames(),
-        /** array of narration objects, OR a promise to return an array of narration objects.
-         * See README for the specfication of the narration objects */
-        // narration: d3promise.csv('app/...filePath.../narration_section_simple.csv'),
-        narration: [{}],
-        /** data in a form consumable by user specified buildGraphFunction method below,
-         * OR a promise to return data. */
-        data: { notEmpty: [] }, // data can't be empty
-        /** promise example... */
-        /** data: d3promise.csv('app/99_example_section_chart/data/data-by-series.csv'), * */
+export const exampleConfigObject = {
+  appContainerId: 'sampleConfigId',
+  /** can override the defaults in this class to customize CSS */
+  cssNames: new ScrollyTellerNames(),
+  /** build a list of story sections, keyed by sectionIdentifier.
+   * Each section constructor should return a valid configuration,
+   * or create a new section "object" outside, and add a .config() function
+   * that returns a valid configuration object */
+  sectionList: [
+    {
+      appContainerId: 'sampleConfigId', /** must be the same as the appContainerId above */
+      sectionIdentifier: 'mySectionIdentifier',
+      /** optional cssNames object if any CSS are overridden from the defaults
+       * can override the defaults in this class to customize CSS */
+      cssNames: new ScrollyTellerNames(),
+      /** array of narration objects, OR a promise to return an array of narration objects.
+       * See README for the specfication of the narration objects */
+      // narration: d3promise.csv('app/...filePath.../narration_section_simple.csv'),
+      narration: [{}],
+      /** data in a form consumable by user specified buildGraphFunction method below,
+       * OR a promise to return data. */
+      data: { notEmpty: [] }, // data can't be empty
+      /** promise example... */
+      /** data: d3promise.csv('app/99_example_section_chart/data/data-by-series.csv'), * */
 
-        /** optional function to reshape data after queries or parsing from a file */
-        reshapeDataFunction(data) { return data; },
+      /** optional function to reshape data after queries or parsing from a file */
+      reshapeDataFunction(data) { return data; },
 
-        /** functions that must be implemented/defined */
-        functionBindingContext: this,
-        /**
-         * Build graph in this function
-         */
-        buildGraphFunction() { /* build chart in this function */ },
-        /**
-         * Called whenever the document is scrolled. Handle scrolling in a function like
-         * the one shown below
-         * @param index - index of the narration block being activated
-         * @param progress - progress from 0-1 of scrolling through that narration block
-         * @param activeNarrationBlock - the DOM element representing the active narration block
-         */
-        onScrollFunction(index, progress, activeNarrationBlock) {
-          /* handle scrolling in this function */
-        },
-        /**
-         * Called when each narration block is activated. Handle in a function like
-         * the one shown below
-         * @param index - index of the narration block being activated
-         * @param activeNarrationBlock - the DOM element representing the active narration block
-         */
-        onActivateNarrationFunction(index, activeNarrationBlock) {
-          /* handle narration in this function */
-        },
-
-        /** optional flags to govern spacers and css behavior */
-
-        /** set to true to show spacer sizes for debugging */
-        showSpacers: true,
-        /**  if false, you must specify your own graph css, where
-         * the graph class name is "graph_section_ + sectionIdentifier" */
-        useDefaultGraphCSS: true,
+      /** functions that must be implemented/defined */
+      functionBindingContext: this,
+      /**
+       * Build graph in this function
+       */
+      buildGraphFunction() { /* build chart in this function */ },
+      /**
+       * Called whenever the document is scrolled. Handle scrolling in a function like
+       * the one shown below
+       * @param index - index of the narration block being activated
+       * @param progress - progress from 0-1 of scrolling through that narration block
+       * @param activeNarrationBlock - the DOM element representing the active narration block
+       */
+      onScrollFunction(index, progress, activeNarrationBlock) {
+        /* handle scrolling in this function */
       },
-      /** another section object like the one above can be added here... */
-    ],
-  };
-}
+      /**
+       * Called when each narration block is activated. Handle in a function like
+       * the one shown below
+       * @param index - index of the narration block being activated
+       * @param activeNarrationBlock - the DOM element representing the active narration block
+       */
+      onActivateNarrationFunction(index, activeNarrationBlock) {
+        /* handle narration in this function */
+      },
+
+      /** optional flags to govern spacers and css behavior */
+
+      /** set to true to show spacer sizes for debugging */
+      showSpacers: true,
+      /**  if false, you must specify your own graph css, where
+       * the graph class name is "graph_section_ + sectionIdentifier" */
+      useDefaultGraphCSS: true,
+    },
+    /** another section object like the one above can be added here... */
+  ],
+};
 
 
 export function isPromise(value) {
