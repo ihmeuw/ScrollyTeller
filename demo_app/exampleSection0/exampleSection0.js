4acdce2190fae250f512bdeceea90c5ef1f83dbb
Ryan Shackleton <ryshack@uw.edu>
2018-08-06 19:09:48 -0700

    Make the demo app explain how triggers work.

--- demo_app/exampleSection0/exampleSection0.js
+++ demo_app/exampleSection0/exampleSection0.js
@@ -4,39 +4,27 @@ import { forIn } from 'lodash';
 import './data/narrationExampleSection0.csv';
 
 const snippets = {
-  onActivate:
+  scrollyTellerIsAwesome:
     `{ // SECTION CONFIGURATION OBJECT
     // ... other properties
-    onActivateNarrationFunction: function ({ trigger, graphId }) {
-        const [first, second] = trigger.split(':');
-
-        switch (first) {
-          case 'opacity':
-            select(\`#\${graphId}\`).style('opacity', +second);
-            break;
-          case 'backgroundColor':
-            select(\`#\${graphId}\`).style('background', second);
-            break;
-          default:
-        }
-      },
+    onActivateNarrationFunction: function ({ trigger, state, graphId }) {
+      console.log('The text based trigger is: ', trigger);
+      
+      if (state.percentOfScrollyTellerThatIsAwesome < 100) {
+        console.log('Wrong! 100% of ScrollyTeller is awesome!!!');
+      }
+    },
   } // SECTION CONFIGURATION OBJECT`,
   onScroll:
     `{ // SECTION CONFIGURATION OBJECT
       // ... other properties
-      onScrollFunction: function ({ progress, trigger, graphId }) {
-        /** use trigger specified in the narration csv file to trigger actions */
-        switch (trigger) {
-          case 'graph:fadein':
-            /** set graph opacity based on progress to fade graph in */
-            select(\`#\${graphId}\`).style('opacity', progress);
-            break;
-          case 'graph:fadeout':
-            /** set graph opacity based on progress to fade graph out */
-            select(\`#\${graphId}\`).style('opacity', 1 - progress);
-            break;
-          default:
-        },
+      onScrollFunction: function ({ progress, trigger, state, graphId }) {
+        console.log('My progress (0-1) is: ', progress);
+        
+        if (state.xStart && state.xEnd) {
+          const currentX = (state.xEnd - state.xStart) * progress;
+          // ... render the graph up to the current x value
+        }
     } // SECTION CONFIGURATION OBJECT`,
 };
 
