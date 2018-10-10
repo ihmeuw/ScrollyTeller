3068e64259ebf655d14e9e72de68860d927eeb27
Ryan Shackleton <ryshack@uw.edu>
2018-10-10 16:16:15 -0700

    Only support last 2 versions. The other stuff is largely unnecessary.

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   plugins: [
-    require('autoprefixer')({ grid: true, browsers: ['last 2 versions', 'ie 10-11', 'Firefox > 20'] })
+    require('autoprefixer')({ grid: true, browsers: ['last 2 versions'] })
   ]
 }
