f676749a345d30d984ec6e9a0f75fd3ae6b59577
Ryan Shackleton <ryshack@uw.edu>
2019-08-29 22:09:21 -0700

    switch to treating trigger_string as json instead of dot notation

--- src/ScrollyTeller/utils/triggerUtils.js
+++ src/ScrollyTeller/utils/triggerUtils.js
@@ -1,4 +1,6 @@
-import { set, reduce, split, slice, replace, isString } from 'lodash-es';
+import {
+  merge, reduce, slice, replace, isString,
+} from 'lodash-es';
 
 function replaceMap(string, replacements) {
   if (!isString(string)) {
@@ -17,26 +19,14 @@ function replaceMap(string, replacements) {
 }
 
 export function getStateFromTrigger(sectionConfig, triggerString, attributes, state = {}) {
-  const {
-    triggerListSeparator = ';',
-    triggerKeyValueSeparator = ':',
-  } = sectionConfig;
-
-  const triggerList = split(triggerString, triggerListSeparator);
-
-  return reduce(triggerList, (results, trigger) => {
-    const [key, value] = split(trigger, triggerKeyValueSeparator);
-
-    if (value === undefined) {
-      results._otherTriggers = results._otherTriggers || [];
-      results._otherTriggers.push(key);
-
-      return results;
-    }
-
-    return set(results, key, replaceMap(value, attributes));
-
-  }, state);
+  const attributeReplacedTrigger = replaceMap(triggerString, attributes);
+  try {
+    const triggerState = JSON.parse(attributeReplacedTrigger);
+    return merge(state, triggerState);
+  } catch (e) {
+    console.warn(`malformed JSON in trigger ${triggerString}`);
+  }
+  return {};
 }
 
 export function getNarrationState(sectionConfig, activeIndex, currentProgress) {
