98f2a767c0f2054fa7862280791402b9dab242ea
Ryan Shackleton <ryshack@uw.edu>
2019-10-24 10:29:27 -0700

    Other features to support demo app display.

--- demo_app/utils.js
+++ demo_app/utils.js
@@ -1,9 +1,10 @@
+import { isNil } from 'lodash-es';
 import { select } from 'd3';
 
-export const highlightLines = (baseCssSelector, linesArray, highlightColor) => {
+export const highlightLines = (baseSelection, linesArray, highlightColor) => {
   if (linesArray instanceof Array) {
     linesArray.forEach((line) => {
-      select(`${baseCssSelector} li:nth-child(${line})`)
+      baseSelection.select(`li:nth-child(${line})`)
         .style('background', highlightColor, 'important')
         .style('border-radius', '5px');
     });
@@ -14,6 +15,13 @@ export const updateSvgImage = (graphId, state, previousSvgFileName) => {
   const {
     svgFileName,
   } = state;
+  if (isNil(svgFileName)) {
+    const graph = select(`#${graphId} .imageDiv`);
+    graph
+      .transition()
+      .duration(250)
+      .style('opacity', 0);
+  }
   if (svgFileName !== previousSvgFileName) {
     const html = svgFileName
       ? `<img src="dist/images/${svgFileName}.svg" />`
@@ -21,13 +29,13 @@ export const updateSvgImage = (graphId, state, previousSvgFileName) => {
     const graph = select(`#${graphId} .imageDiv`);
     graph
       .transition()
-      .duration(500)
+      .duration(250)
       .style('opacity', 0)
       .on('end', () => {
         graph
           .html(html)
           .transition()
-          .duration(500)
+          .duration(250)
           .style('opacity', svgFileName ? 1 : 0);
       });
   }
